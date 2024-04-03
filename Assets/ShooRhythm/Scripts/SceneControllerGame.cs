dbb403911ccc4c70ce28bd43b22dbb56477e3c6e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-03 21:43:29 +0900

    プレイヤープレハブとスクリプトシーケンスを追加

--- Assets/ShooRhythm/Scripts/SceneControllerGame.cs
+++ Assets/ShooRhythm/Scripts/SceneControllerGame.cs
@@ -1,4 +1,7 @@
+using Cysharp.Threading.Tasks;
+using R3;
 using UnityEngine;
+using UnitySequencerSystem;
 
 namespace ShooRhythm
 {
@@ -7,5 +10,31 @@ namespace ShooRhythm
     /// </summary>
     public sealed class SceneControllerGame : MonoBehaviour
     {
+        [SerializeField]
+        private Actor playerPrefab;
+
+        [SerializeField]
+        private ScriptableSequences scriptableSequences;
+
+
+        async UniTask Start()
+        {
+            await BootSystem.IsReady;
+
+            var player = Instantiate(playerPrefab);
+            var inputActions = new InputActions();
+            inputActions.Enable();
+            inputActions.InGame.Fire.OnPerformedAsObservable()
+                .Subscribe(_ =>
+                {
+                    var container = new Container();
+                    container.Register("Actor", player);
+                    container.Register("Actor", player.transform);
+                    var sequencer = new Sequencer(container, scriptableSequences.Sequences);
+                    sequencer.PlayAsync(destroyCancellationToken).Forget();
+                    Debug.Log("Fire");
+                })
+                .RegisterTo(destroyCancellationToken);
+        }
     }
 }
