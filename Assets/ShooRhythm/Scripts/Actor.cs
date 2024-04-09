d1d025b5bf286787c6b1a4213de67cabe46a18bc
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-09 22:42:35 +0900

    Actorクラスに変更を追加***

--- Assets/ShooRhythm/Scripts/Actor.cs
+++ Assets/ShooRhythm/Scripts/Actor.cs
@@ -1,4 +1,7 @@
+using Cysharp.Threading.Tasks;
+using HK;
 using UnityEngine;
+using UnitySequencerSystem;
 
 namespace ShooRhythm
 {
@@ -7,5 +10,33 @@ namespace ShooRhythm
     /// </summary>
     public sealed class Actor : MonoBehaviour
     {
+        [SerializeField]
+        private ScriptableSequences onClickSequences;
+
+        [SerializeField]
+        private Transform animationTarget;
+
+        void Awake()
+        {
+            RegisterAsync().Forget();
+        }
+
+        public void OnClick()
+        {
+            var container = new Container();
+            container.Register("AnimationTarget", animationTarget);
+            var sequencer = new Sequencer(container, onClickSequences.Sequences);
+            sequencer.PlayAsync(destroyCancellationToken).Forget();
+        }
+
+        private async UniTaskVoid RegisterAsync()
+        {
+            var actorManager = await TinyServiceLocator.ResolveAsync<ActorManager>();
+            var p = transform.position;
+            var key = new Vector2Int((int)p.x, (int)p.y);
+            actorManager.AddActor(key, this);
+            await UniTask.WaitUntilCanceled(destroyCancellationToken);
+            actorManager.RemoveActor(key);
+        }
     }
 }
