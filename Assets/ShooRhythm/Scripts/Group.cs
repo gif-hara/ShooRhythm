017c311dd864d213def5fe5842ceaca3d7d1c5e0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-09 23:49:13 +0900

    Group.csの追加と設定の変更

---
+++ Assets/ShooRhythm/Scripts/Group.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShooRhythm
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public abstract class Group<TKey, TValue>
+    {
+        [Serializable]
+        public class Element
+        {
+            public TKey Key;
+
+            public List<TValue> Value;
+        }
+
+        protected Func<TValue, TKey> keySelector;
+
+        [SerializeField]
+        private List<Element> list = new List<Element>();
+
+        public Group(Func<TValue, TKey> keySelector)
+        {
+            this.keySelector = keySelector;
+        }
+
+        public void Set(IEnumerable<TValue> values)
+        {
+            list.Clear();
+            foreach (var value in values)
+            {
+                var key = keySelector(value);
+                var element = list.Find(x => x.Key.Equals(key));
+                if (element == null)
+                {
+                    element = new Element { Key = key, Value = new List<TValue>() };
+                    list.Add(element);
+                }
+                element.Value.Add(value);
+            }
+        }
+    }
+}
