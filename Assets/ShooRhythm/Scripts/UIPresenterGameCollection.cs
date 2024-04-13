fb53c37abc8e7d8ca4540bb70d63334a1dfd68e8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-13 14:40:29 +0900

    UIPresenterGameCollectionの追加と実装

---
+++ Assets/ShooRhythm/Scripts/UIPresenterGameCollection.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using R3;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ShooRhythm
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class UIPresenterGameCollection
+    {
+        public async UniTask BeginAsync(HKUIDocument documentPrefab, CancellationToken cancellationToken)
+        {
+            var document = Object.Instantiate(documentPrefab);
+            var elementParent = document.Q<Transform>("ListElementParent");
+            var elementPrefab = document.Q<HKUIDocument>("ListElementPrefab");
+            var elements = new List<(int id, GameObject gameObject)>();
+            foreach (var i in TinyServiceLocator.Resolve<MasterData>().CollectionSpecs.List)
+            {
+                var element = Object.Instantiate(elementPrefab, elementParent);
+                element.Q<TMP_Text>("Text").text = i.GetItem().Name;
+                element.Q<Button>("Button").OnClickAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        Debug.Log($"Clicked: {i.Id}");
+                    })
+                    .RegisterTo(element.destroyCancellationToken);
+                elements.Add((i.Id, element.gameObject));
+            }
+
+            await UniTask.WaitUntilCanceled(cancellationToken);
+
+            if (document != null)
+            {
+                Object.Destroy(document.gameObject);
+            }
+        }
+    }
+}
