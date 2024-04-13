a7655c0f72bfba2b77193bfcd78e20a12640e3c1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-13 16:49:01 +0900

    GameDataクラスにアイテムの追加と取得のメソッドを追加しました

--- Assets/ShooRhythm/Scripts/GameData.cs
+++ Assets/ShooRhythm/Scripts/GameData.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using HK;
 using SCD;
 
 namespace ShooRhythm
@@ -8,5 +10,18 @@ namespace ShooRhythm
     public sealed class GameData
     {
         public Stats Stats { get; } = new Stats();
+
+        private readonly Dictionary<int, int> items = new();
+
+        public void SetItem(int id, int count)
+        {
+            items[id] = count;
+            TinyServiceLocator.Resolve<GameMessage>().UpdatedItem.OnNext((id, count));
+        }
+
+        public int GetItem(int id)
+        {
+            return items.TryGetValue(id, out var count) ? count : 0;
+        }
     }
 }
