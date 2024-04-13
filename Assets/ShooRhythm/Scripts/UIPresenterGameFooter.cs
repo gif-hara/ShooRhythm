d3105378204ee5069ae1a2af1d1f7ce5b040ce80
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-13 17:14:27 +0900

    UIPresenterGameFooter.csの変更: リスト要素を作成する機能を追加

--- Assets/ShooRhythm/Scripts/UIPresenterGameFooter.cs
+++ Assets/ShooRhythm/Scripts/UIPresenterGameFooter.cs
@@ -1,7 +1,10 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
+using R3;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ShooRhythm
 {
@@ -13,12 +16,28 @@ namespace ShooRhythm
         public async UniTask BeginAsync(HKUIDocument documentPrefab, CancellationToken cancellationToken)
         {
             var document = Object.Instantiate(documentPrefab);
+            var listElementParent = document.Q<Transform>("ListElementParent");
+            var listElementPrefab = document.Q<HKUIDocument>("ListElementPrefab");
+            CreateListElement("道具");
+            CreateListElement("採集");
 
             await UniTask.WaitUntilCanceled(cancellationToken);
 
             if (document != null)
             {
                 Object.Destroy(document.gameObject);
+            }
+
+            void CreateListElement(string text)
+            {
+                var element = Object.Instantiate(listElementPrefab, listElementParent);
+                element.Q<TMP_Text>("Text").text = text;
+                element.Q<Button>("Button").OnClickAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        Debug.Log(text);
+                    })
+                    .RegisterTo(element.destroyCancellationToken);
             }
         }
     }
