26c4200f19bbaa4ed2cc3a2379330f74a8c82ef5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-13 16:49:16 +0900

    GameDataの変更を追加*** ***GameDataの変更を追加

--- Assets/ShooRhythm/Scripts/GameController.cs
+++ Assets/ShooRhythm/Scripts/GameController.cs
@@ -1,4 +1,8 @@
+using System.Threading;
 using HK;
+using R3;
+using SCD;
+using UnityEngine;
 
 namespace ShooRhythm
 {
@@ -7,6 +11,24 @@ namespace ShooRhythm
     /// </summary>
     public sealed class GameController
     {
+        public GameController(CancellationToken cancellationToken)
+        {
+            var gameData = TinyServiceLocator.Resolve<GameData>();
+            Observable.FromEvent<Stats.Record>(
+                x => gameData.Stats.OnChanged += x,
+                x => gameData.Stats.OnChanged -= x
+            )
+                .Subscribe(x =>
+                {
+                    var startString = "Item.";
+                    if (x.Name.StartsWith(startString, System.StringComparison.Ordinal))
+                    {
+                        var id = int.Parse(x.Name.Substring(startString.Length));
+                        gameData.SetItem(id, x.Value);
+                    }
+                })
+                .RegisterTo(cancellationToken);
+        }
         public void AddStats(string name, int value)
         {
             TinyServiceLocator.Resolve<GameData>().Stats.Add(name, value);
