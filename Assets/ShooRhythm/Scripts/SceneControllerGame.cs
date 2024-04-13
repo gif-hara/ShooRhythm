378f20bfba19719c01c199645b73a92bda90b260
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-13 11:31:01 +0900

    SceneControllerGame.csの変更: カメラ関連のフィールドとメソッドを削除

--- Assets/ShooRhythm/Scripts/SceneControllerGame.cs
+++ Assets/ShooRhythm/Scripts/SceneControllerGame.cs
@@ -1,9 +1,6 @@
 using Cysharp.Threading.Tasks;
 using HK;
-using R3;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnitySequencerSystem;
 
 namespace ShooRhythm
 {
@@ -12,24 +9,6 @@ namespace ShooRhythm
     /// </summary>
     public sealed class SceneControllerGame : MonoBehaviour
     {
-        [SerializeField]
-        private Camera controllerdCamera;
-
-        [SerializeField]
-        private float mouseSensitivity = 0.1f;
-
-        [SerializeField]
-        private float scrollSensitivity = 0.1f;
-
-        [SerializeField]
-        private float cameraSizeMin = 1.0f;
-
-        [SerializeField]
-        private float cameraSizeMax = 10.0f;
-
-        private Vector2 lastMousePosition;
-
-
         async UniTask Start()
         {
             await TinyServiceLocator.Resolve<BootSystem>().IsReady;
