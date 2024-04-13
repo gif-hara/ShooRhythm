3d230eaee7767562d6e71204589a78ca79e884d1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-13 15:51:42 +0900

    ShooRhythmのDefine.csを追加しました

---
+++ Assets/ShooRhythm/Scripts/Define.cs
@@ -0,0 +1,15 @@
+namespace ShooRhythm
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Define
+    {
+        public enum CollectionType
+        {
+            None = 0,
+            Collection = 1,
+            Production = 2,
+        }
+    }
+}
