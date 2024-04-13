abe16a577ad6231da9118c1162071fe353d1174f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-13 13:37:04 +0900

    UIPresenterGameIndexクラスを追加しました

---
+++ Assets/ShooRhythm/Scripts/UIPresenterGameIndex.cs
@@ -0,0 +1,25 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using UnityEngine;
+
+namespace ShooRhythm
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class UIPresenterGameIndex
+    {
+        public async UniTask BeginAsync(HKUIDocument documentPrefab, CancellationToken cancellationToken)
+        {
+            var document = Object.Instantiate(documentPrefab);
+
+            await UniTask.WaitUntilCanceled(cancellationToken);
+
+            if (document != null)
+            {
+                Object.Destroy(document.gameObject);
+            }
+        }
+    }
+}
