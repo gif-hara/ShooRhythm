53c79a10b8d0c05e4c3ecd2e506ea5fea0cca98e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-14 17:25:11 +0900

    UIPresenterDraggableItemsの追加と修正

---
+++ Assets/ShooRhythm/Scripts/UIPresenterDraggableItems.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using R3;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ShooRhythm
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class UIPresenterDraggableItems
+    {
+        public UniTask BeginAsync(HKUIDocument document, CancellationToken cancellationToken)
+        {
+            var listElementParentName = "ListElementParent";
+            var elementParent = document.Q<RectTransform>(listElementParentName);
+            var parentLayout = document.Q<GridLayoutGroup>(listElementParentName);
+            var elementPrefab = document.Q<HKUIDocument>("ListElementPrefab");
+            parentLayout.SetConstraintCount();
+            var elements = new List<(int id, GameObject gameObject)>();
+            var specs = TinyServiceLocator.Resolve<MasterData>().CollectionSpecs.List
+                .Where(x => x.CollectionType == Define.CollectionType.Collection);
+            foreach (var i in TinyServiceLocator.Resolve<GameData>().Items)
+            {
+                var element = Object.Instantiate(elementPrefab, elementParent);
+                var masterDataItem = TinyServiceLocator.Resolve<MasterData>().Items.Get(i.Key);
+                element.Q<TMP_Text>("Text.Name").text = masterDataItem.Name;
+                element.Q<TMP_Text>("Text.Number").text = i.Value.ToString();
+                element.Q<Button>("Button").OnClickAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        Debug.Log($"Item.{i.Key}");
+                    })
+                    .RegisterTo(element.destroyCancellationToken);
+                elements.Add((i.Key, element.gameObject));
+            }
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
