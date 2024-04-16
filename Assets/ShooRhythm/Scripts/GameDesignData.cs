736894e3584daef1e6c264a31ca44ef6b98b0484
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-16 16:58:50 +0900

    釣りタブ実装

--- Assets/ShooRhythm/Scripts/GameDesignData.cs
+++ Assets/ShooRhythm/Scripts/GameDesignData.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ShooRhythm
 {
@@ -16,13 +17,33 @@ namespace ShooRhythm
         [Serializable]
         public class FishingDesignData
         {
+            /// <summary>
+            /// ヒット状態になるまでの最小待機時間（秒）
+            /// </summary>
             [SerializeField]
             private float waitSecondsMin;
             public float WaitSecondsMin => waitSecondsMin;
             
+            /// <summary>
+            /// ヒット状態になるまでの最大待機時間（秒）
+            /// </summary>
             [SerializeField]
             private float waitSecondsMax;
             public float WaitSecondsMax => waitSecondsMax;
+            
+            /// <summary>
+            /// ヒットしてから釣り上げられる猶予（秒）
+            /// </summary>
+            [SerializeField]
+            private float postponementSeconds;
+            public float PostponementSeconds => postponementSeconds;
+
+            /// <summary>
+            /// 獲得できる収集Specの名前
+            /// </summary>
+            [SerializeField]
+            private string acquireCollectionSpecName;
+            public string AcquireCollectionSpecName => acquireCollectionSpecName;
         }
     }
 }
--- Assets/ShooRhythm/Scripts/UIPresenterGameFishing.cs
+++ Assets/ShooRhythm/Scripts/UIPresenterGameFishing.cs
@@ -1,6 +1,8 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
+using R3;
+using R3.Triggers;
 using UnityEngine;
 
 namespace ShooRhythm
@@ -13,12 +15,87 @@ namespace ShooRhythm
         public async UniTask BeginAsync(HKUIDocument documentPrefab, CancellationToken cancellationToken)
         {
             var document = Object.Instantiate(documentPrefab);
+            var stateMachine = new TinyStateMachine();
+            var gameDesignData = TinyServiceLocator.Resolve<GameDesignData>();
+            var rootCastButton = document.Q("Root.CastButton");
+            var rootStrikeButton = document.Q("Root.StrikeButton");
+            var rootHitIcon = document.Q("Root.HitIcon");
+            stateMachine.Change(StateIdle);
            
             await UniTask.WaitUntilCanceled(cancellationToken);
 
             if (document != null && document.gameObject != null)
             {
                 Object.Destroy(document.gameObject);
+            }
+
+            UniTask StateIdle(CancellationToken scope)
+            {
+                rootCastButton.SetActive(true);
+                rootStrikeButton.SetActive(false);
+                
+                document.Q<ObservablePointerClickTrigger>("Button.Cast").OnPointerClickAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        stateMachine.Change(StateCast);
+                    })
+                    .RegisterTo(scope);
+                return UniTask.CompletedTask;
+            }
+            
+            UniTask StateCast(CancellationToken scope)
+            {
+                var hitSeconds = Random.Range(
+                    gameDesignData.RiverFishingDesignData.WaitSecondsMin,
+                    gameDesignData.RiverFishingDesignData.WaitSecondsMax
+                    );
+                var currentSeconds = 0f;
+                rootCastButton.SetActive(false);
+                rootStrikeButton.SetActive(true);
+                Observable.EveryUpdate(UnityFrameProvider.Update, scope)
+                    .Subscribe(_ =>
+                    {
+                        currentSeconds += Time.deltaTime;
+                        if(currentSeconds >= hitSeconds)
+                        {
+                            stateMachine.Change(StateHit);
+                        }
+                    })
+                    .RegisterTo(scope);
+                document.Q<ObservablePointerClickTrigger>("Button.Strike").OnPointerClickAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        stateMachine.Change(StateIdle);
+                    })
+                    .RegisterTo(scope);
+                return UniTask.CompletedTask;
+            }
+            
+            async UniTask StateHit(CancellationToken scope)
+            {
+                var postponementSeconds = gameDesignData.RiverFishingDesignData.PostponementSeconds;
+                var currentSeconds = 0f;
+                rootHitIcon.SetActive(true);
+                Observable.EveryUpdate(UnityFrameProvider.Update, scope)
+                    .Subscribe(_ =>
+                    {
+                        currentSeconds += Time.deltaTime;
+                        if(currentSeconds >= postponementSeconds)
+                        {
+                            stateMachine.Change(StateIdle);
+                        }
+                    })
+                    .RegisterTo(scope);
+                document.Q<ObservablePointerClickTrigger>("Button.Strike").OnPointerClickAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        var collectionRecord = TinyServiceLocator.Resolve<MasterData>().Collections.Get(gameDesignData.RiverFishingDesignData.AcquireCollectionSpecName);
+                        TinyServiceLocator.Resolve<GameController>().CollectingAsync(collectionRecord).Forget();
+                        stateMachine.Change(StateIdle);
+                    })
+                    .RegisterTo(scope);
+                await UniTask.WaitUntilCanceled(scope);
+                rootHitIcon.SetActive(false);
             }
         }
     }
