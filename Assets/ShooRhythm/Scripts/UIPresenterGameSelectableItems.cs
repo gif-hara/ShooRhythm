42876cdb244df0c925e1fb2f87941ca7477ae3bd
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-16 14:48:31 +0900

    リファクタリング

--- Assets/ShooRhythm/Scripts/UIPresenterGameSelectableItems.cs
+++ Assets/ShooRhythm/Scripts/UIPresenterGameSelectableItems.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
@@ -26,8 +25,6 @@ namespace ShooRhythm
             var elementPrefab = document.Q<HKUIDocument>("ListElementPrefab");
             parentLayout.SetConstraintCount();
             var elements = new List<(int id, GameObject gameObject)>();
-            var specs = TinyServiceLocator.Resolve<MasterData>().CollectionSpecs.List
-                .Where(x => x.CollectionType == Define.CollectionType.Collection);
             foreach (var i in TinyServiceLocator.Resolve<GameData>().Items)
             {
                 var element = Object.Instantiate(elementPrefab, elementParent);
