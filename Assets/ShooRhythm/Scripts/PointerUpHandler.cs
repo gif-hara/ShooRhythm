dfa1293db2f1530185626094460b1cc5f52ef101
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-16 12:37:33 +0900

    実装

--- Assets/ShooRhythm/Scripts/PointerUpHandler.cs
+++ Assets/ShooRhythm/Scripts/PointerUpHandler.cs
@@ -7,13 +7,13 @@ namespace ShooRhythm
     /// <summary>
     /// 
     /// </summary>
-    public sealed class PointerUpHandler : MonoBehaviour, IPointerDownHandler
+    public sealed class PointerUpHandler : MonoBehaviour, IPointerUpHandler
     {
         private readonly Subject<PointerEventData> onHandled = new();
         
         public Observable<PointerEventData> OnHandledAsObservable() => onHandled;
         
-        public void OnPointerDown(PointerEventData eventData)
+        public void OnPointerUp(PointerEventData eventData)
         {
             onHandled.OnNext(eventData);
         }
