5960ef5d894b357a453e59f1d76be0f5a87c3b00
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-19 00:43:44 +0900

    update farm

--- Assets/ShooRhythm/Scripts/UIPresenterGameFarm.cs
+++ Assets/ShooRhythm/Scripts/UIPresenterGameFarm.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
@@ -5,6 +8,7 @@ using R3;
 using R3.Triggers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ShooRhythm
 {
@@ -15,31 +19,88 @@ namespace ShooRhythm
     {
         public async UniTask BeginAsync(HKUIDocument documentPrefab, CancellationToken cancellationToken)
         {
-            var document = Object.Instantiate(documentPrefab);
+            var document = UnityEngine.Object.Instantiate(documentPrefab);
             var listElementParent = document.Q<Transform>("ListElementParent");
             var listElementPrefab = document.Q<HKUIDocument>("ListElementPrefab");
             var gameData = TinyServiceLocator.Resolve<GameData>();
+            var gameController = TinyServiceLocator.Resolve<GameController>();
             var selectedPlantId = -1;
             var alreadyShowSelectItems = false;
-            for (var i = 0; i < gameData.Stats.Get("Farm.PlantNumber"); i++)
+            var updateProgressScopes = new List<CancellationTokenSource>();
+            for (var i = 0; i < gameData.FarmDatas.Count; i++)
             {
                 var plantId = i;
-                var listElement = Object.Instantiate(listElementPrefab, listElementParent);
-                gameData.Stats.OnChangedAsObservable(cancellationToken)
+                var listElement = UnityEngine.Object.Instantiate(listElementPrefab, listElementParent);
+                var farmData = gameData.FarmDatas[plantId];
+                updateProgressScopes.Add(null);
+                farmData.SeedItemId
                     .Subscribe(x =>
                     {
-                        if (x.Name == $"Farm.Plant.{plantId}.ItemId")
+                        if (farmData.IsCompleted)
                         {
-                            var masterDataItem = TinyServiceLocator.Resolve<MasterData>().Items.Get(x.Value);
-                            listElement.Q<TextMeshProUGUI>("Name").text = masterDataItem.Name;
+                            listElement.Q<TextMeshProUGUI>("Name").text =
+                                TinyServiceLocator.Resolve<MasterData>().Items.Get(farmData.SeedSpec.AcquireItemId).Name;
+                        }
+                        else
+                        {
+                            TinyServiceLocator.Resolve<MasterData>().Items.TryGetValue(x, out var masterDataItem);
+                            listElement.Q<TextMeshProUGUI>("Name").text = masterDataItem?.Name ?? "Empty";
                         }
                     })
-                    .RegisterTo(cancellationToken);
-                listElement.Q<ObservablePointerClickTrigger>("Button").OnPointerClickAsObservable()
-                    .Subscribe(_ =>
+                    .RegisterTo(listElement.destroyCancellationToken);
+                farmData.PlantTicks
+                    .Subscribe(x =>
                     {
-                        selectedPlantId = plantId;
-                        ShowSelectItems();
+
+                        updateProgressScopes[plantId]?.Cancel();
+                        updateProgressScopes[plantId]?.Dispose();
+                        if (x == 0)
+                        {
+                            listElement.Q<Slider>("Progress").value = 0;
+                        }
+                        else
+                        {
+                            if (farmData.IsCompleted)
+                            {
+                                listElement.Q<Slider>("Progress").value = 1;
+                                return;
+                            }
+                            var start = farmData.PlantTicks.Value;
+                            var end = new DateTime(farmData.PlantTicks.Value).AddSeconds(farmData.SeedSpec.GrowSeconds).Ticks;
+                            updateProgressScopes[plantId] = CancellationTokenSource.CreateLinkedTokenSource(
+                                cancellationToken,
+                                listElement.destroyCancellationToken
+                            );
+                            Observable.EveryUpdate(cancellationToken)
+                                .Subscribe(_ =>
+                                {
+                                    var progress = (float)(DateTime.UtcNow.Ticks - start) / (end - start);
+                                    listElement.Q<Slider>("Progress").value = (float)progress;
+                                    if (progress >= 1)
+                                    {
+                                        updateProgressScopes[plantId]?.Cancel();
+                                        updateProgressScopes[plantId]?.Dispose();
+                                        updateProgressScopes[plantId] = null;
+                                        var masterDataItem = TinyServiceLocator.Resolve<MasterData>().Items.Get(farmData.SeedSpec.AcquireItemId);
+                                        listElement.Q<TextMeshProUGUI>("Name").text = masterDataItem.Name;
+                                    }
+                                })
+                                .RegisterTo(updateProgressScopes[plantId].Token);
+                        }
+                    })
+                    .RegisterTo(listElement.destroyCancellationToken);
+                listElement.Q<ObservablePointerClickTrigger>("Button").OnPointerClickAsObservable()
+                    .SubscribeAwait(async (_, ct) =>
+                    {
+                        if (farmData.IsCompleted)
+                        {
+                            await gameController.AcquireFarmPlantAsync(plantId);
+                        }
+                        else
+                        {
+                            selectedPlantId = plantId;
+                            ShowSelectItems();
+                        }
                     })
                     .AddTo(listElement);
             }
@@ -48,7 +109,7 @@ namespace ShooRhythm
 
             if (document != null)
             {
-                Object.Destroy(document.gameObject);
+                UnityEngine.Object.Destroy(document.gameObject);
             }
 
             void ShowSelectItems()
@@ -60,11 +121,17 @@ namespace ShooRhythm
                 alreadyShowSelectItems = true;
                 var selectItemsDocument = document.Q<HKUIDocument>("SelectItems");
                 var uiPresenterSelectItems = new UIPresenterGameSelectItems();
-                uiPresenterSelectItems.BeginAsync(selectItemsDocument, x => x).Forget();
+                uiPresenterSelectItems.BeginAsync(
+                    selectItemsDocument,
+                    x =>
+                    {
+                        return x.Where(pair => TinyServiceLocator.Resolve<MasterData>().SeedSpecs.ContainsKey(pair.Key));
+                    })
+                    .Forget();
                 uiPresenterSelectItems.OnSelectedItemAsObservable()
                     .SubscribeAwait(async (itemId, ct) =>
                     {
-                        await TinyServiceLocator.Resolve<GameController>().SetStatsAsync($"Farm.Plant.{selectedPlantId}.ItemId", itemId);
+                        await gameController.SetFarmPlantItemIdAsync(selectedPlantId, itemId);
                     })
                     .RegisterTo(cancellationToken);
             }
