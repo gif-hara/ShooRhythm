88f1c36023187b46b6499e471140773ee0862e3b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-20 16:28:27 +0900

    農場のメドウプレハブのボタン名を更新

--- Assets/ShooRhythm/Scripts/Extensions.MasterData.cs
+++ Assets/ShooRhythm/Scripts/Extensions.MasterData.cs
@@ -14,6 +14,22 @@ namespace ShooRhythm
             return TinyServiceLocator.Resolve<MasterData>().Items.Get(self.Id);
         }
 
+        public static MasterData.Item GetAcquireItem(this MasterData.NewCollectionSpec self)
+        {
+            return TinyServiceLocator.Resolve<MasterData>().Items.Get(self.AcquireItemId);
+        }
+
+        public static Contents.Record ToContentsRecord(this MasterData.NewCollectionSpec self)
+        {
+            return new Contents.Record(
+                self.Id.ToString(),
+                new List<Stats.Record>(),
+                new List<Stats.Record>(),
+                new List<Stats.Record>(),
+                new List<Stats.Record> { new($"Item.{self.AcquireItemId}", self.AcquireItemAmount) }
+            );
+        }
+
         public static Contents.Record ToContentsRecord(this MasterData.MeadowSpec self)
         {
             return new Contents.Record(
