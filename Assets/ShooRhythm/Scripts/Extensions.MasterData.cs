c495657af8fa8b4535bfe4640159e7f29bb86c5c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-20 16:52:19 +0900

    マスターデータのアイテムIDを更新

--- Assets/ShooRhythm/Scripts/Extensions.MasterData.cs
+++ Assets/ShooRhythm/Scripts/Extensions.MasterData.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using HK;
 using SCD;
 
@@ -9,6 +10,26 @@ namespace ShooRhythm
     /// </summary>
     public static partial class Extensions
     {
+        public static Stats.Record ToStatsRecord(this MasterData.StatsData self)
+        {
+            return new Stats.Record(self.Name, self.Amount);
+        }
+
+        public static Stats.Record ToStatsRecordSubtraction(this MasterData.StatsData self)
+        {
+            return new Stats.Record(self.Name, -self.Amount);
+        }
+
+        public static IEnumerable<Stats.Record> ToStatsRecords(this IEnumerable<MasterData.StatsData> self)
+        {
+            return self.Select(x => x.ToStatsRecord());
+        }
+
+        public static IEnumerable<Stats.Record> ToStatsRecordsSubtraction(this IEnumerable<MasterData.StatsData> self)
+        {
+            return self.Select(x => x.ToStatsRecordSubtraction());
+        }
+
         public static MasterData.Item GetItem(this MasterData.CollectionSpec self)
         {
             return TinyServiceLocator.Resolve<MasterData>().Items.Get(self.Id);
@@ -27,6 +48,24 @@ namespace ShooRhythm
                 new List<Stats.Record>(),
                 new List<Stats.Record>(),
                 new List<Stats.Record> { new($"Item.{self.AcquireItemId}", self.AcquireItemAmount) }
+            );
+        }
+
+        public static List<MasterData.StatsData> GetProductionCondition(this MasterData.ProductionSpec self)
+        {
+            return TinyServiceLocator.Resolve<MasterData>().ProductionConditions.Get(self.Id);
+        }
+
+        public static Contents.Record ToContentsRecord(this MasterData.ProductionSpec self)
+        {
+            var rewards = new List<Stats.Record>(self.GetProductionCondition().ToStatsRecordsSubtraction());
+            rewards.Add(new($"Item.{self.AcquireItemId}", self.AcquireItemAmount));
+            return new Contents.Record(
+                self.Id.ToString(),
+                new List<Stats.Record>(),
+                new List<Stats.Record>(),
+                self.GetProductionCondition().ToStatsRecords().ToList(),
+                rewards
             );
         }
 
