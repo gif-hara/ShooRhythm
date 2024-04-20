38267fa1297c8612ebd6e2492c0f17e56d7f9d61
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-20 15:09:04 +0900

    農場のメドウ仕様に関する拡張メソッドを追加

--- Assets/ShooRhythm/Scripts/Extensions.MasterData.cs
+++ Assets/ShooRhythm/Scripts/Extensions.MasterData.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using HK;
+using SCD;
 
 namespace ShooRhythm
 {
@@ -10,6 +12,24 @@ namespace ShooRhythm
         public static MasterData.Item GetItem(this MasterData.CollectionSpec self)
         {
             return TinyServiceLocator.Resolve<MasterData>().Items.Get(self.Id);
+        }
+
+        public static Contents.Record ToContentsRecord(this MasterData.MeadowSpec self)
+        {
+            return new Contents.Record(
+                self.Id.ToString(),
+                new List<Stats.Record>(),
+                new List<Stats.Record>(),
+                new List<Stats.Record>
+                {
+                    new($"Item.{self.NeedItemId}", self.NeedItemAmount)
+                },
+                new List<Stats.Record>
+                {
+                    new($"Item.{self.AcquireItemId}", self.AcquireItemAmount),
+                    new($"Item.{self.NeedItemId}", -self.NeedItemAmount)
+                }
+            );
         }
     }
 }
