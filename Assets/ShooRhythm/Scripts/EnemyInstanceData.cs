a53758147ca3daef58189a7b42566bf4a58cb1c3
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-21 09:53:13 +0900

    敵のインスタンスデータクラスを追加

---
+++ Assets/ShooRhythm/Scripts/EnemyInstanceData.cs
@@ -0,0 +1,12 @@
+namespace ShooRhythm
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class EnemyInstanceData
+    {
+        public int EnemyId;
+
+        public int HitPoint;
+    }
+}
