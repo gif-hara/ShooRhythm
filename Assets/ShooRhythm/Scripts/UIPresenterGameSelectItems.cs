763e94ba2e2aef0b995fc8fcbae85dcde86a15d2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-23 16:44:59 +0900

    動的に生成されるように

--- Assets/ShooRhythm/Scripts/UIPresenterGameSelectItems.cs
+++ Assets/ShooRhythm/Scripts/UIPresenterGameSelectItems.cs
@@ -7,6 +7,7 @@ using R3;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 namespace ShooRhythm
 {
@@ -25,28 +26,45 @@ namespace ShooRhythm
             var parentLayout = document.Q<GridLayoutGroup>(listElementParentName);
             var elementPrefab = document.Q<HKUIDocument>("ListElementPrefab");
             parentLayout.SetConstraintCount();
-            var elements = new List<(int id, GameObject gameObject)>();
-            foreach (var i in itemSelector(TinyServiceLocator.Resolve<GameData>().Items))
-            {
-                var element = UnityEngine.Object.Instantiate(elementPrefab, elementParent);
-                var masterDataItem = TinyServiceLocator.Resolve<MasterData>().Items.Get(i.Key);
-                element.Q<TMP_Text>("Text.Name").text = masterDataItem.Name;
-                i.Value
-                    .Subscribe(itemNumber =>
-                    {
-                        element.Q<TMP_Text>("Text.Number").text = itemNumber.ToString();
-                    })
-                    .RegisterTo(element.destroyCancellationToken);
-                element.Q<Button>("Button").OnClickAsObservable()
-                    .Subscribe(_ =>
-                    {
-                        onSelectedItem.OnNext(i.Key);
-                    })
-                    .RegisterTo(element.destroyCancellationToken);
-                elements.Add((i.Key, element.gameObject));
-            }
+            var elements = new List<GameObject>();
+            CreateElements();
+            
+            TinyServiceLocator.Resolve<GameMessage>().AddedItem
+                .Subscribe(_ =>
+                {
+                    CreateElements();
+                })
+                .RegisterTo(document.destroyCancellationToken);
 
             return UniTask.CompletedTask;
+
+            void CreateElements()
+            {
+                foreach (var i in elements)
+                {
+                    Object.Destroy(i);
+                }
+                elements.Clear();
+                foreach (var i in itemSelector(TinyServiceLocator.Resolve<GameData>().Items))
+                {
+                    var element = UnityEngine.Object.Instantiate(elementPrefab, elementParent);
+                    var masterDataItem = TinyServiceLocator.Resolve<MasterData>().Items.Get(i.Key);
+                    element.Q<TMP_Text>("Text.Name").text = masterDataItem.Name;
+                    i.Value
+                        .Subscribe(itemNumber =>
+                        {
+                            element.Q<TMP_Text>("Text.Number").text = itemNumber.ToString();
+                        })
+                        .RegisterTo(element.destroyCancellationToken);
+                    element.Q<Button>("Button").OnClickAsObservable()
+                        .Subscribe(_ =>
+                        {
+                            onSelectedItem.OnNext(i.Key);
+                        })
+                        .RegisterTo(element.destroyCancellationToken);
+                    elements.Add(element.gameObject);
+                }
+            }
         }
     }
 }
