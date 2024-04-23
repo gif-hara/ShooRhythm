9e73fb9d063445fe7a6fcf4a5418157fbd98ce3d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-23 23:24:17 +0900

    クールタイムのUIとユーザーデータの処理を修正

--- Assets/ShooRhythm/Scripts/UIPresenterGameFooter.cs
+++ Assets/ShooRhythm/Scripts/UIPresenterGameFooter.cs
@@ -2,6 +2,7 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
 using R3;
+using R3.Triggers;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,11 +17,18 @@ namespace ShooRhythm
         public async UniTask BeginAsync(HKUIDocument documentPrefab, CancellationToken cancellationToken)
         {
             var document = Object.Instantiate(documentPrefab);
-            var listElementParent = document.Q<Transform>("ListElementParent");
-            var listElementPrefab = document.Q<HKUIDocument>("ListElementPrefab");
+            var gameData = TinyServiceLocator.Resolve<GameData>();
+            var buttonListElementParent = document.Q<Transform>("ButtonListElementParent");
+            var buttonListElementPrefab = document.Q<HKUIDocument>("ButtonListElementPrefab");
+            var coolTimeListElementParent = document.Q<Transform>("CoolTimeListElementParent");
+            var coolTimeListElementPrefab = document.Q<HKUIDocument>("CoolTimeListElementPrefab");
             foreach (var i in TinyServiceLocator.Resolve<GameDesignData>().Footers)
             {
-                CreateListElement(i.FooterName, i.TabType, i.ActiveStatsName);
+                CreateButtonElement(i.FooterName, i.TabType, i.ActiveStatsName);
+            }
+            for (var i = 0; i < gameData.CurrentUserData.coolTimeData.Count; i++)
+            {
+                CreateCoolTimeElement(i);
             }
 
             await UniTask.WaitUntilCanceled(cancellationToken);
@@ -30,23 +38,41 @@ namespace ShooRhythm
                 Object.Destroy(document.gameObject);
             }
 
-            void CreateListElement(string text, Define.TabType tabType, string isActiveStatsName)
+            void CreateButtonElement(string text, Define.TabType tabType, string isActiveStatsName)
             {
-                var element = Object.Instantiate(listElementPrefab, listElementParent);
+                var element = Object.Instantiate(buttonListElementPrefab, buttonListElementParent);
                 element.Q<TMP_Text>("Text").text = text;
-                element.Q<Button>("Button").OnClickAsObservable()
+                element.Q<ObservablePointerClickTrigger>("Button").OnPointerClickAsObservable()
                     .Subscribe(_ =>
                     {
                         TinyServiceLocator.Resolve<GameMessage>().RequestChangeTab.OnNext(tabType);
                     })
                     .RegisterTo(element.destroyCancellationToken);
-                var gameData = TinyServiceLocator.Resolve<GameData>();
                 element.gameObject.SetActiveIfNeed(gameData.Stats.Contains(isActiveStatsName));
                 gameData.Stats.OnChangedAsObservable()
                     .Where(x => x.Name == isActiveStatsName)
                     .Subscribe(x =>
                     {
                         element.gameObject.SetActiveIfNeed(gameData.Stats.Contains(isActiveStatsName));
+                    })
+                    .RegisterTo(element.destroyCancellationToken);
+            }
+
+            void CreateCoolTimeElement(int index)
+            {
+                var element = Object.Instantiate(coolTimeListElementPrefab, coolTimeListElementParent);
+                var slider = element.Q<Slider>("Slider");
+                var coolTimeData = gameData.CurrentUserData.coolTimeData[index];
+                coolTimeData.CoolTime
+                    .Subscribe(x =>
+                    {
+                        var max = coolTimeData.Max;
+                        if (max == 0)
+                        {
+                            slider.value = 1;
+                            return;
+                        }
+                        slider.value = (coolTimeData.Max - x) / coolTimeData.Max;
                     })
                     .RegisterTo(element.destroyCancellationToken);
             }
