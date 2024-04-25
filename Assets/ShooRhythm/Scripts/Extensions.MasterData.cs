96660b6e77ef470caeb11cc8ae10f827f1e09525
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-25 23:57:35 +0900

    アイテムの獲得処理を修正

--- Assets/ShooRhythm/Scripts/Extensions.MasterData.cs
+++ Assets/ShooRhythm/Scripts/Extensions.MasterData.cs
@@ -12,26 +12,6 @@ namespace ShooRhythm
     /// </summary>
     public static partial class Extensions
     {
-        public static Stats.Record ToStatsRecord(this MasterData.StatsData self)
-        {
-            return new Stats.Record(self.Name, self.Amount);
-        }
-
-        public static Stats.Record ToStatsRecordSubtraction(this MasterData.StatsData self)
-        {
-            return new Stats.Record(self.Name, -self.Amount);
-        }
-
-        public static IEnumerable<Stats.Record> ToStatsRecords(this IEnumerable<MasterData.StatsData> self)
-        {
-            return self.Select(x => x.ToStatsRecord());
-        }
-
-        public static IEnumerable<Stats.Record> ToStatsRecordsSubtraction(this IEnumerable<MasterData.StatsData> self)
-        {
-            return self.Select(x => x.ToStatsRecordSubtraction());
-        }
-
         public static MasterData.Item GetAcquireItem(this MasterData.CollectionSpec self)
         {
             return TinyServiceLocator.Resolve<MasterData>().Items.Get(self.AcquireItemId);
