838ee48e0663c891c9c109a9f879b0232b528489
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-04-26 12:13:49 +0900

    リファクタリング

--- Assets/ShooRhythm/Scripts/GameUtility.cs
+++ Assets/ShooRhythm/Scripts/GameUtility.cs
@@ -14,30 +14,6 @@ namespace ShooRhythm
     /// </summary>
     public sealed class GameUtility
     {
-        public static void ShowContentsConditionsNotification(Contents.Record contentsRecord)
-        {
-            var gameMessage = TinyServiceLocator.Resolve<GameMessage>();
-            foreach (var i in contentsRecord.Conditions)
-            {
-                if (i.Name.StartsWith("Item.", System.StringComparison.Ordinal))
-                {
-                    var itemId = int.Parse(i.Name.Substring(5));
-                    var masterDataItem = TinyServiceLocator.Resolve<MasterData>().Items.Get(itemId);
-                    gameMessage.RequestNotification.OnNext(
-                        (
-                            $"{masterDataItem.Name}が{i.Value}個必要です",
-                            null,
-                            Define.NotificationType.Negative
-                        )
-                    );
-                }
-                else
-                {
-                    Debug.LogWarning($"Unknown record name: {i.Name}");
-                }
-            }
-        }
-        
         public static async UniTask PlayAcquireItemEffectAsync(HKUIDocument document, RectTransform parent, UniTask<Sprite>? iconTask, CancellationToken cancellationToken)
         {
             var effectPrefab = document.Q<HKUIDocument>("AcquireItemEffect");
